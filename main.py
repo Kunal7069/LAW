cb57707e1490366915893897a069065ab9b22a88
Kunal7069 <107999879+Kunal7069@users.noreply.github.com>
2024-12-26 17:45:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
-
+port = 9000
 folder_path = "./files" 
 
 def initialize_model():
@@ -110,6 +110,6 @@ def process_input():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
-if __name__ == "__main__":
-    app.run(debug=True,host='0.0.0.0', port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=port, debug=True)
     
